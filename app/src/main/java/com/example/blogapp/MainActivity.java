11d20eda0f7534f562dc0ce26a457d0c5922857e
deepak <deepakswarnabarath578@gmail.com>
2019-10-21 12:40:37 +0530

    Created some template

--- app/src/main/java/com/example/blogapp/MainActivity.java
+++ app/src/main/java/com/example/blogapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.blogapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -77,17 +80,46 @@ public class MainActivity extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(MainActivity.this,
                                     "Signed In", Toast.LENGTH_SHORT).show();
-                        }else {
 
+//                            startActivity(new Intent(MainActivity.this,
+//                                    PostListActivity.class));
+
+                        }else {
+                            Toast.makeText(MainActivity.this, "Task not successful", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_signout){
+
+            mAuth.signOut();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
